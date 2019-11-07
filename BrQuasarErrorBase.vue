bece04e7c9f7a73378532182ec2c93d1631b1338
David I. Lehn <dlehn@digitalbazaar.com>
2019-11-07 16:19:57 -0500

    Add override for bedrock-vue base error page.

---
+++ BrQuasarErrorBase.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page padding>
+    <h3><slot name="title" /></h3>
+    <slot />
+    <slot name="contact" />
+  </q-page>
+</template>
+<script>
+export default {
+  name: 'BrQuasarErrorBase'
+};
+</script>
+<style>
+</style>
